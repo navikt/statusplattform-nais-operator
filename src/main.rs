df81f7ee952cb6fca4d728a693ea1ca666a54502
x10an14-nav <christian.chavez@nav.no>
2024-04-25 16:11:51 +0200

    bugfix: Check actual ready status on endpoints
    TODO: Figure out how to map this to NAIS apps.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashSet, env};
 
 use futures::TryStreamExt;
-use k8s_openapi::api::core::v1::Service;
+use k8s_openapi::api::discovery::v1::EndpointSlice;
 use kube::{
     api::{Api, ResourceExt},
     runtime::{watcher, WatchStreamExt},
@@ -32,23 +32,23 @@ fn collate_excluded_namespaces(env_vars: &[&str]) -> String {
     env_vals.into_iter().collect::<Vec<_>>().join(",")
 }
 
-/// Returns true if and only if the Service's `spec.clusterIPs`
-///  (as addressed in the YAML spec) is _not_ empty.
-fn service_is_ready(s: &Service) -> bool {
-    let Some(ref service_spec) = s.spec else {
-        return false;
-    };
-    let Some(ref pod_ip_list) = service_spec.cluster_ips else {
-        return false;
-    };
-    !pod_ip_list.is_empty()
+/// Returns true if and only if at least one of the `EndpointSlice`'s
+///  `endpoints[].conditions.ready` evaluate to `true`
+///  Always return false elsewise.
+fn endpointslice_is_ready(endpoint_slice: &EndpointSlice) -> bool {
+    endpoint_slice
+        .endpoints
+        .iter()
+        .flat_map(|e| &e.conditions)
+        .filter_map(|c| c.ready)
+        .any(|is_ready| is_ready)
 }
 
 #[tokio::main]
 async fn main() -> color_eyre::eyre::Result<()> {
     tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
-    let api = Api::<Service>::all(client);
+    let api = Api::<EndpointSlice>::all(client);
     // requires WatchList feature gate on 1.27 or later: TODO check if cluster supports
     let wc = watcher::Config::default()
         .labels("app,team") // I just care if the label(s) exist
@@ -61,7 +61,7 @@ async fn main() -> color_eyre::eyre::Result<()> {
         .try_for_each(|s| async move {
             // s.metadata... // TODO: Ensure owner reference to a nais.io/XXXX Application
             info!("saw {}", s.name_any());
-            match service_is_ready(&s) {
+            match endpointslice_is_ready(&s) {
                 // TODO: Send http request to the statusplattform backend API w/reqwest
                 true => {
                     warn!(
